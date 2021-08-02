96e853ab94b179f7c065ef4a01aa5c465a0ee3df
coder187 <coder187@hotmail.com>
2021-08-02 11:36:13 +0000

    add Create Table function

--- run.py
+++ run.py
@@ -43,5 +43,23 @@ def SelectQueryWithCursor():
         connection.close
         print ("### Select Query with Cursor Completed and db connecton closed")
 
+
+
+def AddTable():
+    try:
+    # Run a query
+        connection = GetConnection()
+        with connection.cursor() as cursor:
+            cursor.execute("""CREATE TABLE IF NOT EXISTS
+                          Friends(name char(20), age int, DOB datetime);""")
+        # Note that the above will still display a warning (not error) if the
+        # table already exists
+    finally:
+        # Close the connection, regardless of whether or not the above was successful
+        connection.close
+        print ("### AddTable Completed and db connecton closed")
+
+
 SelectQuery()
 SelectQueryWithCursor()
+AddTable()
