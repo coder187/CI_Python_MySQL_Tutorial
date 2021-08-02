b046b4c51ffdba4a2e81a279ccd3b59a91ea8aad
coder187 <coder187@hotmail.com>
2021-08-02 11:32:24 +0000

    add select and select with cursor example

--- run.py
+++ run.py
@@ -1,2 +1,47 @@
 import os
 import pymysql
+
+# Get the username from the Cloud9 workspace
+# (modify this variable if running on another environment)
+username = os.getenv('C9_USER')
+
+# Connect to the database
+def GetConnection():
+    print ("opening db connection")
+    return pymysql.connect(host='localhost',
+                             user=username,
+                             password='',
+                             db='Chinook')
+
+
+def SelectQuery():
+    try:
+    # Run a query
+        connection = GetConnection()
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM Artist;"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            print(result)
+    finally:
+        # Close the connection, regardless of whether or not the above was successful
+        connection.close
+        print ("### Select Query Completed and db connecton closed")
+
+
+def SelectQueryWithCursor():
+    try:
+    # Run a query
+        connection = GetConnection()
+        with connection.cursor(pymysql.cursors.DictCursor) as cursor:
+            sql = "SELECT * FROM Genre;"
+            cursor.execute(sql)
+            for row in cursor:
+                print(row)
+    finally:
+        # Close the connection, regardless of whether or not the above was successful
+        connection.close
+        print ("### Select Query with Cursor Completed and db connecton closed")
+
+SelectQuery()
+SelectQueryWithCursor()
